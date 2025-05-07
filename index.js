3352c892ce41553eab93da3e949c46fd348d871d
KevinGomesDev <kevingomesdev2025@gmail.com>
2025-05-07 03:41:51 -0300

    chore(routes): mover rotas de /kingdom para a raiz /

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connect } from './db.js';
-import kingdomRoutes from './routes/kingdomRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connect } from "./db.js";
+import kingdomRoutes from "./routes/kingdomRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -10,13 +10,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Rotas
-app.use('/kingdom', kingdomRoutes);
+app.use("/", kingdomRoutes);
 
 // Inicialização
-connect().then(() => {
-  app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco:", err);
   });
-}).catch(err => {
-  console.error("Erro ao conectar ao banco:", err);
-});
